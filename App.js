b4122072f77ded95082ec974dd0c77e404e4e1f6
Nick Porter <nporter@adobe.com>
2019-12-13 14:13:23 -0700

    Install SDK and test

--- App.js
+++ App.js
@@ -1,114 +1,174 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+@flow
+@format
+*/
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button, ScrollView, NativeModules} from 'react-native';
+import {ACPCore, ACPLifecycle, ACPSignal, ACPIdentity, ACPMobileLogLevel, ACPMobilePrivacyStatus, ACPMobileVisitorAuthenticationState, ACPVisitorID, ACPExtensionEvent} from '@adobe/react-native-acpcore';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView contentContainerStyle={{ marginTop: 75 }}>
+        <Text style={styles.welcome}>ACPCore Test App</Text>
+        <Button title="ACPCore::extensionVersion()" onPress={() => this.coreExtensionVersion()}/>
+        <Button title="ACPLifecycle::extensionVersion()" onPress={this.lifecycleExtensionVersion}/>
+        <Button title="ACPIdentity::extensionVersion()" onPress={this.identityExtensionVersion}/>
+        <Button title="ACPSignal::extensionVersion()" onPress={this.signalExtensionVersion}/>
+        <Button title="ACPCore::setPrivacyStatus(OptIn)" onPress={this.setPrivacyOptIn}/>
+        <Button title="ACPCore::setPrivacyStatus(OptOut)" onPress={this.setPrivacyOptOut}/>
+        <Button title="ACPCore::getPrivacyStatus()" onPress={this.getPrivacyStatus}/>
+        <Button title="ACPCore::getLogLevel()" onPress={this.getLogLevel}/>
+        <Button title="ACPCore::log(...)" onPress={this.log}/>
+        <Button title="ACPCore::getSdkIdentities()" onPress={this.getSdkIdentities}/>
+        <Button title="ACPCore::setAdvertisingIdentifier(adID)" onPress={this.setAdvertisingIdentifier}/>
+        <Button title="ACPCore::lifecycleStart()" onPress={this.lifecycleStart}/>
+        <Button title="ACPCore::lifecyclePause()" onPress={this.lifecyclePause}/>
+        <Button title="ACPCore::collectPii()" onPress={this.collectPii}/>
+        <Button title="ACPCore::dispatchEvent()" onPress={this.dispatchEvent}/>
+        <Button title="ACPCore::dispatchEventWithResponseCallback()" onPress={this.dispatchEventWithResponseCallback}/>
+        <Button title="ACPCore::dispatchResponseEvent()" onPress={this.dispatchResponseEvent}/>
+
+        <Button title="ACPIdentity::syncIdentifiers()" onPress={this.syncIdentifiers}/>
+        <Button title="ACPIdentity::syncIdentifiersWithAuthState()" onPress={this.syncIdentifiersWithAuthState}/>
+        <Button title="ACPIdentity::syncIdentifier()" onPress={this.syncIdentifier}/>
+        <Button title="ACPIdentity::appendVisitorInfoForURL()" onPress={this.appendVisitorInfoForURL}/>
+        <Button title="ACPIdentity::getUrlVariables()" onPress={this.getUrlVariables}/>
+        <Button title="ACPIdentity::getIdentifiers()" onPress={this.getIdentifiers}/>
+        <Button title="ACPIdentity::getExperienceCloudId()" onPress={this.getExperienceCloudId}/>
         </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+      </View>
+    );
+  }
+
+  coreExtensionVersion() {
+    ACPCore.extensionVersion().then(version => console.log("AdobeExperienceSDK: ACPCore version: " + version));
+  }
+
+  lifecycleExtensionVersion() {
+    ACPLifecycle.extensionVersion().then(version => console.log("AdobeExperienceSDK: ACPLifecycle version: " + version));
+  }
+
+  identityExtensionVersion() {
+    ACPIdentity.extensionVersion().then(version => console.log("AdobeExperienceSDK: ACPIdentity version: " + version));
+  }
+
+  signalExtensionVersion() {
+    ACPSignal.extensionVersion().then(version => console.log("AdobeExperienceSDK: ACPSignal version: " + version));
+  }
+
+
+  setPrivacyOptIn() {
+    ACPCore.setPrivacyStatus(ACPMobilePrivacyStatus.OPT_IN);
+  }
+
+  setPrivacyOptOut() {
+    ACPCore.setPrivacyStatus(ACPMobilePrivacyStatus.OPT_OUT);
+  }
+
+  getPrivacyStatus() {
+    ACPCore.getPrivacyStatus().then(status => console.log("AdobeExperienceSDK: Privacy Status = " + status));
+  }
+
+  getLogLevel() {
+    ACPCore.getLogLevel().then(level => console.log("AdobeExperienceSDK: Log Level = " + level));
+  }
+
+  log() {
+    ACPCore.log(ACPMobileLogLevel.VERBOSE, "React Native Tag", "React Native Message");
+  }
+
+  getSdkIdentities() {
+    ACPCore.getSdkIdentities().then(identities => console.log("AdobeExperienceSDK: Identities = " + identities));
+  }
+
+  setAdvertisingIdentifier() {
+    ACPCore.setAdvertisingIdentifier("adID");
+  }
+
+  lifecycleStart() {
+    ACPCore.lifecycleStart({"lifecycleStart": "myData"});
+  }
+
+  lifecyclePause() {
+    ACPCore.lifecyclePause();
+  }
+
+  collectPii() {
+    ACPCore.collectPii({"myPii": "data"});
+  }
+
+  dispatchEvent() {
+    var event = new ACPExtensionEvent("eventName", "eventType", "eventSource", {"testDataKey": "testDataValue"});
+    ACPCore.dispatchEvent(event);
+  }
+
+  dispatchEventWithResponseCallback() {
+    var event = new ACPExtensionEvent("eventName", "eventType", "eventSource", {"testDataKey": "testDataValue"});
+    ACPCore.dispatchEventWithResponseCallback(event).then(responseEvent => console.log("AdobeExperienceSDK: responseEvent = " + responseEvent));
+  }
+
+  dispatchResponseEvent() {
+    var responseEvent = new ACPExtensionEvent("responseEvent", "eventType", "eventSource", {"testDataKey": "testDataValue"});
+    var requestEvent = new ACPExtensionEvent("requestEvent", "eventType", "eventSource", {"testDataKey": "testDataValue"});
+    ACPCore.dispatchResponseEvent(responseEvent, requestEvent);
+  }
+
+  // identity
+
+  syncIdentifiers() {
+    ACPIdentity.syncIdentifiers({"id1": "identifier1"});
+  }
+
+  syncIdentifiersWithAuthState() {
+    ACPIdentity.syncIdentifiersWithAuthState({"id1": "identifier1"}, "ACP_VISITOR_AUTH_STATE_AUTHENTICATED");
+  }
+
+  syncIdentifier() {
+    ACPIdentity.syncIdentifier("idType", "ID", ACPMobileVisitorAuthenticationState.AUTHENTICATED);
+  }
+
+  appendVisitorInfoForURL() {
+    ACPIdentity.appendVisitorInfoForURL("test.com").then(urlWithVisitorData => console.log("AdobeExperienceSDK: VisitorData = " + urlWithVisitorData));
+  }
+
+  getUrlVariables() {
+    ACPIdentity.getUrlVariables().then(urlVariables => console.log("AdobeExperienceSDK: UrlVariables = " + urlVariables));
+  }
+
+  getIdentifiers() {
+    ACPIdentity.getIdentifiers().then(identifiers => console.log("AdobeExperienceSDK: Identifiers = " + identifiers));
+  }
+
+  getExperienceCloudId() {
+    ACPIdentity.getExperienceCloudId().then(cloudId => console.log("AdobeExperienceSDK: CloudID = " + cloudId));
+  }
+
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  welcome: {
+    fontSize: 22,
+    textAlign: 'center',
+    margin: 10,
+  }
 });
-
-export default App;
--- android/app/src/main/java/com/gitissue54/MainApplication.java
+++ android/app/src/main/java/com/gitissue54/MainApplication.java
@@ -9,6 +9,13 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.adobe.marketing.mobile.MobileCore; // import MobileCore
+import com.adobe.marketing.mobile.MobileCore;
+import com.adobe.marketing.mobile.Identity;
+import com.adobe.marketing.mobile.Lifecycle;
+import com.adobe.marketing.mobile.Signal;
+import com.adobe.marketing.mobile.Analytics;
+import com.adobe.marketing.mobile.WrapperType;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -43,6 +50,20 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    MobileCore.setApplication(this);
+    MobileCore.configureWithAppID("yourAppId");
+    MobileCore.setWrapperType(WrapperType.REACT_NATIVE);
+    try {
+      Identity.registerExtension();
+      Lifecycle.registerExtension();
+      Signal.registerExtension();
+      Analytics.registerExtension();
+      // Register any additional extensions
+    } catch (Exception e) {
+      // handle exception
+    }
+
+    MobileCore.start(null);
     initializeFlipper(this); // Remove this line if you don't want Flipper enabled
   }
 
